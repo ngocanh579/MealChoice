c7a70ed4a498046c5b1a6bb0d966bc905b44bf83
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-07-31 01:20:58 +0700

    update merchant entity

--- src/main/java/vn/codegyme/meal_choice/entity/Merchant.java
+++ src/main/java/vn/codegyme/meal_choice/entity/Merchant.java
@@ -1,5 +1,40 @@
 package vn.codegyme.meal_choice.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
+
+
+@Entity
+@Table(name = "merchants")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Merchant {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "restaurant_name")
+    private String restaurantName;
+
+    private String email;
+    private String phone;
+    private String address;
+
+    @Column(name ="open_time")
+    private LocalTime openTime;
+
+    @Column(name ="close_time")
+    private LocalTime closeTime;
+
+    @Column(name ="merchant_status")
+    private String merchantStatus;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
