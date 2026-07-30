30ed44502f08112320619fa9b071c16a04e2f3e7
Hai Trieu <haitriuwork30@gmail.com>
2026-07-30 21:24:32 +0700

    feat: add GlobalExceptionHandler for central error handling

--- src/main/java/vn/codegyme/meal_choice/exception/GlobalExceptionHandler.java
+++ src/main/java/vn/codegyme/meal_choice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,33 @@
 package vn.codegyme.meal_choice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    // Handle all throw runtime error (wrong password, email,...)
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    // Handle validation error
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        // Get the readable error message only
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message));
+    }
 }
