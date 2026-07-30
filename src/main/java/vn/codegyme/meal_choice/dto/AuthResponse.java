ef961dfbb6aec0938c7a7da91d8001d00e2e64a4
Hai Trieu <haitriuwork30@gmail.com>
2026-07-30 21:50:40 +0700

    Feat: create AuthResponse

---
+++ src/main/java/vn/codegyme/meal_choice/dto/AuthResponse.java
@@ -0,0 +1,29 @@
+package vn.codegyme.meal_choice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AuthResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private UUID id;
+
+    private String email;
+
+    private String displayName;
+
+    private String phoneNumber;
+
+    private String gender;
+}
