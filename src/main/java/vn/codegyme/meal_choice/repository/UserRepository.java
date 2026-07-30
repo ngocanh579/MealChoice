91d88b085b18689882f32bcbf8da467924cefac4
Hai Trieu <haitriuwork30@gmail.com>
2026-07-31 00:55:50 +0700

    Feat: create UserRepository for login

--- src/main/java/vn/codegyme/meal_choice/repository/UserRepository.java
+++ src/main/java/vn/codegyme/meal_choice/repository/UserRepository.java
@@ -1,5 +1,17 @@
 package vn.codegyme.meal_choice.repository;
 
-public class UserRepository {
+import org.hibernate.boot.models.JpaAnnotations;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.codegyme.meal_choice.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
     
+    boolean existsByEmail(String email);
 }
