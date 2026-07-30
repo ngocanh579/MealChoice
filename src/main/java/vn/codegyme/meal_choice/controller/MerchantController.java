718c4a37844145dad9406ae23388266d5e744b6c
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-07-31 01:20:03 +0700

    update MerchantController for merchant register

--- src/main/java/vn/codegyme/meal_choice/controller/MerchantController.java
+++ src/main/java/vn/codegyme/meal_choice/controller/MerchantController.java
@@ -1,5 +1,26 @@
 package vn.codegyme.meal_choice.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.codegyme.meal_choice.dto.MerchantRegisterRequest;
+
+import vn.codegyme.meal_choice.service.MerchantService;
+
+@RestController
+@RequestMapping("/api/merchants")
+@RequiredArgsConstructor
 public class MerchantController {
-    
+    private final MerchantService merchantService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerMerchant(@Valid @RequestBody MerchantRegisterRequest request) {
+        merchantService.registerMerchant(request);
+        return ResponseEntity.ok("Đăng ký thành công");
+    }
+
 }
