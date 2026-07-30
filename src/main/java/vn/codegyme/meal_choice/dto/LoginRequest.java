662f411c5600af77de1c9bc7f3a2736441ef859a
Hai Trieu <haitriuwork30@gmail.com>
2026-07-30 21:44:06 +0700

    Feat: create LoginRequest DTO

--- src/main/java/vn/codegyme/meal_choice/dto/LoginRequest.java
+++ src/main/java/vn/codegyme/meal_choice/dto/LoginRequest.java
@@ -1,5 +1,24 @@
 package vn.codegyme.meal_choice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
-    
+
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không hơợp lệ")
+    private String email;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,}$",
+            message = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm cả chữ và số"
+    )
+    private String password;
 }
