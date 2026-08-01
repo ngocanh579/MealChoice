0e9c55027ebde7f0f1fb84abb5c4f142024634bf
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-02 00:26:16 +0700

    update merchant service

--- src/main/java/vn/codegyme/meal_choice/service/MerchantService.java
+++ src/main/java/vn/codegyme/meal_choice/service/MerchantService.java
@@ -1,6 +1,5 @@
 package vn.codegyme.meal_choice.service;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,10 +13,12 @@ import vn.codegyme.meal_choice.repository.MerchantRepository;
 import vn.codegyme.meal_choice.repository.RoleRepository;
 import vn.codegyme.meal_choice.repository.UserRepository;
 
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class MerchantService {
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final MerchantRepository merchantRepository;
@@ -25,7 +26,7 @@ public class MerchantService {
     private final EmailService emailService;
 
     @Transactional
-    public void registerMerchant(MerchantRegisterRequest request){
+    public void registerMerchant(MerchantRegisterRequest request) {
 
         if (userRepository.existsByEmail(request.getEmail())) {
             throw new RuntimeException("Email đã tồn tại");
@@ -41,24 +42,26 @@ public class MerchantService {
 
         User user = new User();
 
-        user.setName(request.getOwnerName());
+        user.setDisplayName(request.getOwnerName());
         user.setEmail(request.getEmail());
         user.setPhoneNumber(request.getPhone());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
+
         // Tạm thời active tài khoản ngay.
         // TODO: Đổi thành false khi hoàn thiện chức năng kích hoạt qua email.
         user.setIsActive(true);
 
-        // lấy role_Merchant
+        // Lấy ROLE_MERCHANT
         Role merchantRole = roleRepository
                 .findByName(Role.RoleName.ROLE_MERCHANT)
                 .orElseThrow(() ->
                         new RuntimeException("Không tìm thấy ROLE_MERCHANT"));
+
         user.getRoles().add(merchantRole);
 
         userRepository.save(user);
 
-        // tạo Merchant
+        // Tạo Merchant
         Merchant merchant = new Merchant();
 
         merchant.setRestaurantName(request.getRestaurantName());
@@ -69,14 +72,19 @@ public class MerchantService {
         merchant.setUser(user);
 
         merchant.setMerchantStatus("PENDING");
+
         merchantRepository.save(merchant);
+
         emailService.sendMerchantRegisterEmail(
                 request.getEmail(),
                 request.getRestaurantName()
         );
     }
 
-    public void updateMerchant(Long merchantId, MerchantUpdateRequest request) {
+    public void updateMerchant(
+            UUID merchantId,
+            MerchantUpdateRequest request
+    ) {
 
         Merchant merchant = merchantRepository.findById(merchantId)
                 .orElseThrow(() ->
@@ -89,5 +97,4 @@ public class MerchantService {
 
         merchantRepository.save(merchant);
     }
-    
 }
