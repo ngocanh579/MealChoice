1526e628e038ca79108d54fb297d16b7ad8ecf01
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-01 17:58:11 +0700

    configure security for merchant api

--- src/main/java/vn/codegyme/meal_choice/config/SecurityConfig.java
+++ src/main/java/vn/codegyme/meal_choice/config/SecurityConfig.java
@@ -1,5 +1,29 @@
 package vn.codegyme.meal_choice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/merchants/register").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
