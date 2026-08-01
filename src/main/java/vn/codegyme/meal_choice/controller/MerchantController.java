99d899a337a84952c12ebc810e77ea3682c8bc80
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-02 00:26:22 +0700

    update merchant controller

--- src/main/java/vn/codegyme/meal_choice/controller/MerchantController.java
+++ src/main/java/vn/codegyme/meal_choice/controller/MerchantController.java
@@ -5,30 +5,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.codegyme.meal_choice.dto.MerchantRegisterRequest;
-
 import vn.codegyme.meal_choice.dto.MerchantUpdateRequest;
 import vn.codegyme.meal_choice.service.MerchantService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/merchants")
 @RequiredArgsConstructor
 public class MerchantController {
+
     private final MerchantService merchantService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerMerchant(@Valid @RequestBody MerchantRegisterRequest request) {
+    public ResponseEntity<String> registerMerchant(
+            @Valid @RequestBody MerchantRegisterRequest request) {
+
         merchantService.registerMerchant(request);
+
         return ResponseEntity.ok("Đăng ký thành công");
     }
 
     @PutMapping("/{merchantId}")
     public ResponseEntity<String> updateMerchant(
-            @PathVariable Long merchantId,
+            @PathVariable UUID merchantId,
             @Valid @RequestBody MerchantUpdateRequest request) {
 
         merchantService.updateMerchant(merchantId, request);
 
-        return ResponseEntity.ok("Cập nhật thông tin cửa hàng thành công");
+        return ResponseEntity.ok(
+                "Cập nhật thông tin cửa hàng thành công"
+        );
     }
-
 }
