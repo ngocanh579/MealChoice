62914eda49afe49e43b99ecd9ca18d03a9f477f8
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-02 00:26:10 +0700

    update user repository

--- src/main/java/vn/codegyme/meal_choice/repository/UserRepository.java
+++ src/main/java/vn/codegyme/meal_choice/repository/UserRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import vn.codegyme.meal_choice.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByPhoneNumber(String phoneNumber);
 
