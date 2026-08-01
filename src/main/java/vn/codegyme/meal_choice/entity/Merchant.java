cde7f41c894d6a94b34ee98384ce4874fc8c1eaa
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-02 00:25:17 +0700

    update merchant entity

--- src/main/java/vn/codegyme/meal_choice/entity/Merchant.java
+++ src/main/java/vn/codegyme/meal_choice/entity/Merchant.java
@@ -1,11 +1,12 @@
 package vn.codegyme.meal_choice.entity;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
-
+import java.util.UUID;
 
 @Entity
 @Table(name = "merchants")
@@ -13,28 +14,58 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Merchant {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(name = "restaurant_name")
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(
+            name = "restaurant_name",
+            nullable = false,
+            length = 150
+    )
     private String restaurantName;
 
+    @Column(
+            name = "merchant_email",
+            unique = true,
+            nullable = false,
+            length = 255
+    )
     private String email;
+
+    @Column(
+            name = "merchant_phone",
+            unique = true,
+            nullable = false,
+            length = 20
+    )
     private String phone;
+
+    @Column(
+            name = "merchant_address",
+            nullable = false,
+            length = 255
+    )
     private String address;
 
-    @Column(name ="open_time")
+    @Column(name = "merchant_open_time")
     private LocalTime openTime;
 
-    @Column(name ="close_time")
+    @Column(name = "merchant_close_time")
     private LocalTime closeTime;
 
-    @Column(name ="merchant_status")
+    @Column(
+            name = "merchant_status",
+            length = 30
+    )
     private String merchantStatus;
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(
+            name = "user_id",
+            unique = true
+    )
     private User user;
-
 }
