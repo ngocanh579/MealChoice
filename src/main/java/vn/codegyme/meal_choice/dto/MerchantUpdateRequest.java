e3e4aea2b00e108a2c0c669c20b3c668e3f556c0
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-06 18:30:47 +0700

    update merchant update validation

--- src/main/java/vn/codegyme/meal_choice/dto/MerchantUpdateRequest.java
+++ src/main/java/vn/codegyme/meal_choice/dto/MerchantUpdateRequest.java
@@ -1,6 +1,7 @@
 package vn.codegyme.meal_choice.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalTime;
@@ -11,9 +12,12 @@ public class MerchantUpdateRequest {
     @NotBlank(message = "Tên nhà hàng không được để trống")
     private String merchantRestaurantName;
 
+    @NotBlank(message = "Địa chỉ không được để trống")
     private String merchantAddress;
 
+    @NotNull(message = "Giờ mở cửa không được để trống")
     private LocalTime merchantOpenTime;
 
+    @NotNull(message = "Giờ đóng cửa không được để trống")
     private LocalTime merchantCloseTime;
 }
