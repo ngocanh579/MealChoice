37b97de161cddfa5d5a86d15d79f267f76c61cb3
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-10 07:01:47 +0700

    feat(merchant): bổ sung dữ liệu tỉnh huyện phường cho cập nhật

--- src/main/java/vn/codegyme/meal_choice/dto/merchant/MerchantUpdateRequest.java
+++ src/main/java/vn/codegyme/meal_choice/dto/merchant/MerchantUpdateRequest.java
@@ -15,6 +15,15 @@ public class MerchantUpdateRequest {
     @NotBlank(message = "Địa chỉ không được để trống")
     private String merchantAddress;
 
+    @NotBlank(message = "Vui lòng chọn tỉnh/thành phố")
+    private String provinceCode;
+
+    @NotBlank(message = "Vui lòng chọn quận/huyện")
+    private String districtCode;
+
+    @NotBlank(message = "Vui lòng chọn phường/xã")
+    private String wardCode;
+
     @NotNull(message = "Giờ mở cửa không được để trống")
     private LocalTime merchantOpenTime;
 
