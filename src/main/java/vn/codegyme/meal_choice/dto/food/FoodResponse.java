0e329485b1cef85e3a0f6639ebf063b2eb7d15b5
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-10 19:48:26 +0700

    feat: sprint 2 thêm response món ăn

--- src/main/java/vn/codegyme/meal_choice/dto/food/FoodResponse.java
+++ src/main/java/vn/codegyme/meal_choice/dto/food/FoodResponse.java
@@ -1,4 +1,44 @@
 package vn.codegyme.meal_choice.dto.food;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class FoodResponse {
+
+    private UUID id;
+
+    private UUID merchantAddressId;
+
+    private String merchantAddress;
+
+    private String foodName;
+
+    private Integer preparationTime;
+
+    private String foodNote;
+
+    private BigDecimal price;
+
+    private BigDecimal discountPrice;
+
+    private BigDecimal serviceFee;
+
+    private Integer views;
+
+    private Integer orderCount;
+
+    private Boolean isRecommended;
+
+    private List<UUID> categoryIds;
+
+    private List<String> imageUrls;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
