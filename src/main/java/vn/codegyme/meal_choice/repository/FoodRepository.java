ff5b11a1fe726d78af431e9e992707e1d42fce5c
Le Thi Ngoc Anh <ngoc.ltnanh@gmail.com>
2026-08-17 08:53:59 +0700

    Thêm kiểm tra món ăn trước khi xóa địa chỉ

--- src/main/java/vn/codegyme/meal_choice/repository/FoodRepository.java
+++ src/main/java/vn/codegyme/meal_choice/repository/FoodRepository.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
+    boolean existsByMerchantAddress_Id(UUID merchantAddressId);
 
     // Lấy danh sách món của Merchant
     List<Food> findByMerchant_IdAndDeletedAtIsNullOrderByCreatedAtDesc(
