4d28479041569dbfc3fcafc49e38e5ab53c2b12f
Hai Trieu <haitriuwork30@gmail.com>
2026-08-29 14:52:35 +0700

    Feat(cart): create OrderSummaryResponse DTO

--- src/main/java/vn/codegyme/meal_choice/dto/order/OrderSummaryResponse.java
+++ src/main/java/vn/codegyme/meal_choice/dto/order/OrderSummaryResponse.java
@@ -1,4 +1,29 @@
 package vn.codegyme.meal_choice.dto.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderSummaryResponse {
+
+    private UUID id;
+    private String orderCode;
+
+    private UUID merchantId;
+    private String merchantName;
+
+    private int totalItemQuantity; // tổng số lượng món trong đơn
+    private BigDecimal totalAmount;
+
+    private String status;
+    private LocalDateTime createdAt;
 }
