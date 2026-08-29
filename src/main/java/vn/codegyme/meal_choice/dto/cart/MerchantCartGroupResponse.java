e4869be39cf32ea2dcf0909bf133cfb7203f08a8
Hai Trieu <haitriuwork30@gmail.com>
2026-08-29 15:12:54 +0700

    Feat(cart): create MerchantCartGroupResponse DTO

--- src/main/java/vn/codegyme/meal_choice/dto/cart/MerchantCartGroupResponse.java
+++ src/main/java/vn/codegyme/meal_choice/dto/cart/MerchantCartGroupResponse.java
@@ -1,4 +1,26 @@
 package vn.codegyme.meal_choice.dto.cart;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MerchantCartGroupResponse {
+
+    private UUID merchantId;
+    private String merchantName;
+    private boolean merchantAvailable; // false nếu quán bị khóa/từ chối
+
+    private List<CartItemResponse> items;
+
+    private BigDecimal merchantSubtotal;   // Tổng tiền món của quán này
+    private BigDecimal merchantServiceFee; // Tổng phí dịch vụ của quán này
 }
