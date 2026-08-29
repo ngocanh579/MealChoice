fab18f513b33b6264c8ee443d9dec9aa7f18c263
Hai Trieu <haitriuwork30@gmail.com>
2026-08-29 15:08:31 +0700

    Feat(cart): add verify whether deli address belongs user's order

--- src/main/java/vn/codegyme/meal_choice/repository/AddressRepository.java
+++ src/main/java/vn/codegyme/meal_choice/repository/AddressRepository.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Repository;
 import vn.codegyme.meal_choice.entity.Address;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
     List<Address> findByUserId(UUID userId);
+
+    Optional<Address> findByIdAndUser_Id(Long id, UUID userId);
 }
